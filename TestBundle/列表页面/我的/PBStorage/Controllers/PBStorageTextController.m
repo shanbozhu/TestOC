378b8c5b9d3643e8c32d4595c1fae5d5abc8b8cd
zhushanbo <zhushanbo@baidu.com>
2019-09-05 16:11:53 +0800

    no message

--- TestBundle/列表页面/我的/PBStorage/Controllers/PBStorageTextController.m
+++ TestBundle/列表页面/我的/PBStorage/Controllers/PBStorageTextController.m
@@ -22,6 +22,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // 文件路径
+    self.filePath = [PBSandBox absolutePathWithRelativePath:@"/Documents/myar.ar"];
+    
     UIButton *archiverBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:archiverBtn];
     archiverBtn.frame = CGRectMake(20, 100, [UIScreen mainScreen].bounds.size.width-40, 40);
@@ -38,6 +41,7 @@
 }
 
 - (void)archiverBtn:(UIButton *)btn {
+    // 数据
     PBStorageText *testText = [[PBStorageText alloc]init];
     testText.name = @"helloworld";
     testText.age = 1000;
@@ -46,24 +50,21 @@
     NSArray *arr = @[testText, testText];
     NSDictionary *dic = @{@"testText":testText, @"name":@"helloworld", @"testTextArr":@[testText, testText]};
     
+    // 归档
     NSData *data = [PBArchiver dataWithObjects:@[str, arr, dic] keys:@[@"str", @"arr", @"dic"]];
     
-    // 文件路径
-    NSString *filePath = [[PBSandBox path4Documents]stringByAppendingPathComponent:@"myar.ar"];
-    self.filePath = filePath;
-    if (![[NSFileManager defaultManager]fileExistsAtPath:filePath]) {
-        [[NSFileManager defaultManager]createFileAtPath:filePath contents:nil attributes:nil];
-    }
-    
     // 归档数据存储到本地文件
-    [data writeToFile:filePath atomically:YES];
+    [data writeToFile:self.filePath atomically:YES];
 }
 
 - (void)unArchiverBtn:(UIButton *)btn {
     // 从本地文件读取归档数据
     NSMutableData *data = [NSMutableData dataWithContentsOfFile:self.filePath];
     
+    // 解档
     NSArray *objs = [PBArchiver objectsWithData:data keys:@[@"str", @"arr", @"dic"]];
+    
+    // 数据
     NSString *str = objs[0];
     NSArray *arr = objs[1];
     NSDictionary *dic = objs[2];
