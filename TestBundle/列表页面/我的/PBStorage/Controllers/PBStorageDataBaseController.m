e65dff6fff34621d4eb0f87e4b112fb6a99528e6
zhushanbo <zhushanbo@baidu.com>
2019-09-06 18:49:25 +0800

    no message

--- TestBundle/列表页面/我的/PBStorage/Controllers/PBStorageDataBaseController.m
+++ TestBundle/列表页面/我的/PBStorage/Controllers/PBStorageDataBaseController.m
@@ -19,6 +19,8 @@
 @property (nonatomic, copy) NSString *testSid;
 @property (nonatomic, copy) NSString *testColumn;
 
+@property (nonatomic, strong) FMDatabase *db;
+
 @end
 
 @implementation PBStorageDataBaseController
@@ -84,72 +86,68 @@
 
 // 创建表
 - (void)createTable {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    self.db = [FMDatabase databaseWithPath:self.filePath];
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db executeUpdate:@"create table if not exists students(sid TEXT, name TEXT)"]) {
+    if ([self.db executeUpdate:@"create table if not exists students(sid TEXT, name TEXT)"]) {
         NSLog(@"在数据库文件中创建表成功"); // 表名为students,含有两个表字段分别为sid和name
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 增加
 - (void)insert:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db executeUpdate:@"insert into students(sid, name) values(?, ?)", self.testSid, self.testName]) {
+    if ([self.db executeUpdate:@"insert into students(sid, name) values(?, ?)", self.testSid, self.testName]) {
         NSLog(@"增加表中的一条或多条记录成功");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 删除
 - (void)delete:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db executeUpdate:@"delete from students where sid = ?", self.testSid]) {
+    if ([self.db executeUpdate:@"delete from students where sid = ?", self.testSid]) {
         NSLog(@"删除表中的一条或多条记录成功");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 修改
 - (void)update:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db executeUpdate:@"update students set name = '阿祖', sid = '1' where sid = ?", self.testSid]) {
+    if ([self.db executeUpdate:@"update students set name = '阿祖', sid = '1' where sid = ?", self.testSid]) {
         NSLog(@"修改表中的一条或多条记录成功");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 查找
 - (void)select:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
@@ -157,7 +155,7 @@
     //NSString *sql = @"select * from students order by sid DESC"; // 反向输出
     //NSString *sql = @"select name from students where sid = '952'";
     //NSString *sql = @"select * from students where sid = '952'";
-    FMResultSet *result = [db executeQuery:@"select * from students where sid = ?", self.testSid];
+    FMResultSet *result = [self.db executeQuery:@"select * from students where sid = ?", self.testSid];
     while ([result next]) {
         NSString *sid = [result stringForColumn:@"sid"];
         NSString *name = [result stringForColumn:@"name"];
@@ -165,48 +163,46 @@
     }
     NSLog(@"查找表中的一条或多条记录成功");
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 添加表字段
 - (void)alter:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if (![db columnExists:self.testColumn inTableWithName:@"students"]) {
+    if (![self.db columnExists:self.testColumn inTableWithName:@"students"]) {
         NSString *sql = [NSString stringWithFormat:@"alter table students add column %@ TEXT", self.testColumn];
-        if ([db executeUpdate:sql]) {
+        if ([self.db executeUpdate:sql]) {
             NSLog(@"添加表字段成功");
         }
     } else {
         NSLog(@"添加表字段失败,表中已经含有要添加的字段");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 重命名表
 - (void)rename:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db tableExists:@"students"]) {
-        if ([db executeUpdate:@"alter table students rename to tmp"]) {
+    if ([self.db tableExists:@"students"]) {
+        if ([self.db executeUpdate:@"alter table students rename to tmp"]) {
             NSLog(@"重命名表成功");
         }
     } else {
         NSLog(@"重命名表失败,不存在需要重命名的表名");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
@@ -216,37 +212,35 @@
     // 创建表
     [self createTable];
     
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db executeUpdate:@"insert into students select sid, name from tmp"]) {
+    if ([self.db executeUpdate:@"insert into students select sid, name from tmp"]) {
         NSLog(@"复制表成功");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
 
 // 删除表
 - (void)drop:(UIButton *)btn {
-    FMDatabase *db = [FMDatabase databaseWithPath:self.filePath];
-    if (![db open]) {
+    if (![self.db open]) {
         NSLog(@"打开数据库文件失败");
     }
     
-    if ([db tableExists:@"tmp"]) {
+    if ([self.db tableExists:@"tmp"]) {
         NSString *sql = @"drop table tmp";
-        if ([db executeUpdate:sql]) {
+        if ([self.db executeUpdate:sql]) {
             NSLog(@"删除表成功");
         }
     } else {
         NSLog(@"删除表失败,没有需要删除的表");
     }
     
-    if (![db close]) {
+    if (![self.db close]) {
         NSLog(@"关闭数据库文件失败");
     }
 }
