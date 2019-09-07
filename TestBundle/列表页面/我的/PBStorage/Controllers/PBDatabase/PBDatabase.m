08ed39186ce2a3e5b4cb79b84a046588a1ce4754
zhushanbo <zhushanbo@baidu.com>
2019-09-07 12:25:53 +0800

    no message

--- TestBundle/列表页面/我的/PBStorage/Controllers/PBDatabase/PBDatabase.m
+++ TestBundle/列表页面/我的/PBStorage/Controllers/PBDatabase/PBDatabase.m
@@ -11,12 +11,10 @@
 #import "PBSandBox.h"
 #import <pthread.h>
 
-@interface PBDatabase () {
-    pthread_rwlock_t _lock;
-}
+@interface PBDatabase ()
 
 @property (nonatomic, copy) NSString *filePath;
-@property (nonatomic, strong) FMDatabase *db;
+@property (nonatomic, strong) FMDatabaseQueue *dbQueue;
 
 @end
 
@@ -45,14 +43,10 @@ static id sharedDatabase = nil;
 
 - (id)init {
     if (self = [super init]) {
-        pthread_rwlock_init(&_lock, NULL);
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
-        
         // 指定路径创建文件
         self.filePath = [PBSandBox absolutePathWithRelativePath:DATABASEFILEPATH];
         [PBSandBox createFileAtPath:self.filePath];
-        self.db = [FMDatabase databaseWithPath:self.filePath]; // 无法自动创建父目录
+        self.dbQueue = [FMDatabaseQueue databaseQueueWithPath:self.filePath]; // 无法自动创建父目录
         
         // 创建表
         [self createTable];
@@ -60,73 +54,57 @@ static id sharedDatabase = nil;
     return self;
 }
 
-- (void)applicationDidEnterBackground {
-    if (![self.db close]) {
-        NSLog(@"关闭数据库文件失败");
-    }
-}
-
 #pragma mark - 操作
 - (void)createTable {
-    if (![self.db open]) {
-        NSLog(@"打开数据库文件失败");
-    }
-    if (![self.db executeUpdate:@"create table if not exists myTable(key TEXT, value BLOB)"]) {
-        NSLog(@"在数据库文件中创建表失败");
-    }
+    [self.dbQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+        if (![db executeUpdate:@"create table if not exists myTable(key TEXT, value BLOB)"]) {
+            NSLog(@"在数据库文件中创建表失败");
+        }
+    }];
 }
 
 - (void)setValue:(id)value forKey:(NSString *)key {
-    pthread_rwlock_wrlock(&_lock);
-    
-    FMResultSet *result = [self.db executeQuery:@"select * from myTable where key = ?", key];
-    while ([result next]) {
-        if (![self.db executeUpdate:@"delete from myTable where key = ?", key]) {
-            NSLog(@"删除表中的一条或多条记录失败");
+    [self.dbQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+        FMResultSet *result = [db executeQuery:@"select * from myTable where key = ?", key];
+        while ([result next]) {
+            if (![db executeUpdate:@"delete from myTable where key = ?", key]) {
+                NSLog(@"删除表中的一条或多条记录失败");
+            }
         }
-    }
-    if (![self.db executeUpdate:@"insert into myTable(key, value) values(?, ?)", key, [PBArchiver dataWithObject:value key:key]]) {
-        NSLog(@"增加表中的一条或多条记录失败");
-    }
-    
-    pthread_rwlock_unlock(&_lock);
+        if (![db executeUpdate:@"insert into myTable(key, value) values(?, ?)", key, [PBArchiver dataWithObject:value key:key]]) {
+            NSLog(@"增加表中的一条或多条记录失败");
+        }
+    }];
 }
 
 - (void)removeObjectForKey:(NSString *)defaultName {
-    pthread_rwlock_wrlock(&_lock);
-    
-    if (![self.db executeUpdate:@"delete from myTable where key = ?", defaultName]) {
-        NSLog(@"删除表中的一条或多条记录失败");
-    }
-    
-    pthread_rwlock_unlock(&_lock);
+    [self.dbQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+        if (![db executeUpdate:@"delete from myTable where key = ?", defaultName]) {
+            NSLog(@"删除表中的一条或多条记录失败");
+        }
+    }];
 }
 
 - (id)valueForKey:(NSString *)key {
-    pthread_rwlock_rdlock(&_lock);
-    
-    NSData *value;
-    FMResultSet *result = [self.db executeQuery:@"select * from myTable where key = ?", key];
-    while ([result next]) {
-        value = [result dataForColumn:@"value"];
-    }
-    
-    pthread_rwlock_unlock(&_lock);
+    __block NSData *value;
+    [self.dbQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+        FMResultSet *result = [db executeQuery:@"select * from myTable where key = ?", key];
+        while ([result next]) {
+            value = [result dataForColumn:@"value"];
+        }
+    }];
     return [PBArchiver objectWithData:value key:key];
 }
 
 - (void)removeAllObjects {
-    pthread_rwlock_wrlock(&_lock);
-    
-    if (![self.db executeUpdate:@"delete from myTable"]) {
-        NSLog(@"删除表中的一条或多条记录失败");
-    }
-    
-    pthread_rwlock_unlock(&_lock);
+    [self.dbQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+        if (![db executeUpdate:@"delete from myTable"]) {
+            NSLog(@"删除表中的一条或多条记录失败");
+        }
+    }];
 }
 
 - (void)dealloc {
-    pthread_rwlock_destroy(&_lock);
     NSLog(@"PBDatabase对象被释放了");
 }
 
