c10b04b4f7c2d13754fd3d3cceba410a1873e8e6
zhushanbo <zhushanbo@baidu.com>
2022-04-21 13:58:09 +0800

    no message

--- TestOC/Page/Mine/PBArrayDelete/Controllers/PBArrayDeleteController.m
+++ TestOC/Page/Mine/PBArrayDelete/Controllers/PBArrayDeleteController.m
@@ -10,7 +10,6 @@
 
 @interface PBArrayDeleteController ()
 
-
 @end
 
 @implementation PBArrayDeleteController
@@ -19,116 +18,121 @@
     [super viewDidLoad];
 
     {
+        // 正序
         NSMutableArray *arr = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
         for (int i = 0; i < arr.count; i++) {
             NSString *str = arr[i];
             if (![str isEqualToString:@"1"]) {
-                //[arr removeObject:str]; // 不会崩溃,但是删除不干净
-                [arr removeObjectAtIndex:i]; // 不会崩溃,但是删除不干净
+                [arr removeObject:str]; // 不会崩溃,但是删除不干净
+                //[arr removeObjectAtIndex:i]; // 不会崩溃,但是删除不干净
             }
         }
         NSLog(@"arr = %@", arr);
         
-        // 推荐
-        NSMutableArray *arr6 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-        for (int i = (int)arr6.count - 1; i > 0; i--) {
-            NSString *str = arr6[i];
+        // 倒序 推荐
+        NSMutableArray *arr1 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        for (int i = (int)arr1.count - 1; i > 0; i--) {
+            NSString *str = arr1[i];
             if (![str isEqualToString:@"1"]) {
-                [arr6 removeObject:str]; // 支持
-                //[arr6 removeObjectAtIndex:i]; // 支持
+                [arr1 removeObject:str]; // 支持
+                //[arr1 removeObjectAtIndex:i]; // 支持
             }
         }
-        NSLog(@"arr6 = %@", arr6);
+        NSLog(@"arr1 = %@", arr1);
         
-        NSMutableArray *arr9 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-        NSMutableArray *tmpArr = [arr9 mutableCopy];
+        // 临时数组
+        NSMutableArray *arr2 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        NSMutableArray *tmpArr = [arr2 mutableCopy];
         for (int i = 0; i < tmpArr.count; i++) {
             NSString *str = tmpArr[i];
             if (![str isEqualToString:@"1"]) {
-                [arr9 removeObject:str]; // 支持
-                //[arr9 removeObjectAtIndex:i]; // 不会崩溃,但是删除不干净
+                [arr2 removeObject:str]; // 支持
+                //[arr2 removeObjectAtIndex:[arr2 indexOfObject:str]]; // 支持
             }
         }
-        NSLog(@"arr9 = %@", arr9);
+        NSLog(@"arr2 = %@", arr2);
     }
 
     {
-        NSMutableArray *arr1 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-        [arr1 enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        // 正序
+        NSMutableArray *arr = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        [arr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             NSString *str = (NSString *)obj;
             if (![str isEqualToString:@"1"]) {
-                [arr1 removeObject:str]; // 不会崩溃,但是删除不干净
-                //[arr1 removeObjectAtIndex:idx]; // 不会崩溃,但是删除不干净
+                [arr removeObject:str]; // 不会崩溃,但是删除不干净
+                //[arr removeObjectAtIndex:idx]; // 不会崩溃,但是删除不干净
+            }
+        }];
+        NSLog(@"arr = %@", arr);
+        
+        // 倒序 推荐
+        NSMutableArray *arr1 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        [arr1 enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSString *str = (NSString *)obj;
+            if (![str isEqualToString:@"1"]) {
+                [arr1 removeObject:str]; // 支持
+                //[arr1 removeObjectAtIndex:idx]; // 支持
             }
         }];
         NSLog(@"arr1 = %@", arr1);
         
-        // 推荐
-        NSMutableArray *arr7 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-        [arr7 enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *str = (NSString *)obj;
-            if (![str isEqualToString:@"1"]) {
-                [arr7 removeObject:str]; // 支持
-                //[arr7 removeObjectAtIndex:idx]; // 支持
-            }
-        }];
-        NSLog(@"arr7 = %@", arr7);
-        
+        // 临时数组
         NSMutableArray *arr2 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
         NSMutableArray *tmpArr = arr2.mutableCopy;
         [tmpArr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             NSString *str = (NSString *)obj;
             if (![str isEqualToString:@"1"]) {
                 [arr2 removeObject:str]; // 支持
-                //[arr2 removeObjectAtIndex:idx]; // 不会崩溃,但是删除不干净
+                //[arr2 removeObjectAtIndex:[arr2 indexOfObject:str]]; // 支持
             }
         }];
         NSLog(@"arr2 = %@", arr2);
     }
 
     {
-//        NSMutableArray *arr3 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-//        for (NSString *str in arr3) {
-//            if (![str isEqualToString:@"1"]) {
-//                [arr3 removeObject:str]; // 崩溃
-//            }
-//        }
-//        NSLog(@"arr3 = %@", arr3);
-        
-//        NSMutableArray *arr8 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-//        for (NSString *str in arr8.reverseObjectEnumerator) {
-//            if ([str isEqualToString:@"1"]) {
-//                [arr8 removeObject:str]; // 崩溃
-//            }
-//        }
-//        NSLog(@"arr8 = %@", arr8);
-        
-        NSMutableArray *arr4 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
-        NSMutableArray *tmpArr = arr4.mutableCopy;
-        for (NSString *str in tmpArr) {
+        // 正序
+        NSMutableArray *arr = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        for (NSString *str in arr) {
             if (![str isEqualToString:@"1"]) {
-                [arr4 removeObject:str]; // 支持
-                //[arr4 removeObjectAtIndex:[tmpArr indexOfObject:str]]; // 不会崩溃,但是删除不干净
+                //[arr removeObject:str]; // 崩溃
+                //[arr removeObjectAtIndex:[arr indexOfObject:str]]; // 崩溃
             }
         }
-        NSLog(@"arr4 = %@", arr4);
+        NSLog(@"arr = %@", arr);
+        
+        // 倒序
+        NSMutableArray *arr1 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        for (NSString *str in arr1.reverseObjectEnumerator) {
+            if ([str isEqualToString:@"1"]) {
+                //[arr1 removeObject:str]; // 崩溃
+                [arr1 removeObjectAtIndex:[arr1 indexOfObject:str]]; // 支持
+            }
+        }
+        NSLog(@"arr1 = %@", arr1);
+        
+        // 临时数组
+        NSMutableArray *arr2 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+        NSMutableArray *tmpArr = arr2.mutableCopy;
+        for (NSString *str in tmpArr) {
+            if (![str isEqualToString:@"1"]) {
+                [arr2 removeObject:str]; // 支持
+                //[arr2 removeObjectAtIndex:[arr2 indexOfObject:str]]; // 支持
+            }
+        }
+        NSLog(@"arr2 = %@", arr2);
     }
 
     // 推荐
-    NSMutableArray *arr5 = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
+    NSMutableArray *arr = [NSMutableArray arrayWithArray:@[@"1", @"2", @"3", @"1"]];
     NSMutableIndexSet *indexSet = [NSMutableIndexSet indexSet];
-    for (int i = 0; i < arr5.count; i++) {
-        NSString *str = arr5[i];
+    for (int i = 0; i < arr.count; i++) {
+        NSString *str = arr[i];
         if (![str isEqualToString:@"1"]) {
             [indexSet addIndex:i];
         }
     }
-    [arr5 removeObjectsAtIndexes:indexSet];
-    NSLog(@"arr5 = %@", arr5);
+    [arr removeObjectsAtIndexes:indexSet];
+    NSLog(@"arr = %@", arr);
 }
-
-
-
-
 
 @end
