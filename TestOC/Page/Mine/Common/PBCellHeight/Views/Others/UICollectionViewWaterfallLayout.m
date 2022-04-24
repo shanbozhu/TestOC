470a0a560bd87d3b1a0fdedcddb7110b007429dc
zhushanbo <zhushanbo@baidu.com>
2022-04-24 12:16:47 +0800

    no message

--- TestOC/Page/Mine/Common/PBCellHeight/Views/Others/UICollectionViewWaterfallLayout.m
+++ TestOC/Page/Mine/Common/PBCellHeight/Views/Others/UICollectionViewWaterfallLayout.m
@@ -10,9 +10,9 @@
 
 @interface UICollectionViewWaterfallLayout ()
 
-@property (nonatomic, strong) NSMutableArray *attributeArray; // frame 数组
-@property (nonatomic, assign) NSInteger collectViewRowCount;  // 列数
-@property (nonatomic, strong) NSMutableArray *originYAry;    //记录每一列的Y点坐标
+@property (nonatomic, strong) NSMutableArray *attributeArr; // frame 数组
+@property (nonatomic, assign) NSInteger colCount; // 列数
+@property (nonatomic, strong) NSMutableArray *originYArr; //记录每一列的Y点坐标
 
 @end
 
@@ -20,21 +20,21 @@
 
 - (instancetype)init {
     if (self = [super init]) {
-        _attributeArray = [NSMutableArray array];
-        _originYAry = [NSMutableArray array];
-        _collectViewRowCount = 3;
+        self.attributeArr = [NSMutableArray array];
+        self.originYArr = [NSMutableArray array];
+        self.colCount = 3;
     }
     return self;
 }
- 
+
 // 1.告诉当前layout对象，更新当前布局
 - (void)prepareLayout {
-    [_attributeArray removeAllObjects];
-    [_originYAry removeAllObjects];
+    [self.attributeArr removeAllObjects];
+    [self.originYArr removeAllObjects];
     
     //初始化y坐标
-    for (int i = 0; i < _collectViewRowCount; i++) {
-        [_originYAry addObject:@(0)];
+    for (int i = 0; i < self.colCount; i++) {
+        [self.originYArr addObject:@(0)];
     }
     
     //返回
@@ -43,37 +43,37 @@
         NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
         //UICollectionViewLayoutAttributes 用于存储layout属性的对象
         UICollectionViewLayoutAttributes *attrib = [self layoutAttributesForItemAtIndexPath:indexPath];
-        [_attributeArray addObject:attrib];
+        [self.attributeArr addObject:attrib];
     }
 }
- 
+
 // 2.返回指定indexPath下的 UICollectionViewLayoutAttributes
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
     // 根据indexPath创建一个UICollectionViewLayoutAttributes
     UICollectionViewLayoutAttributes *layoutAttr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
     
     // 设置每个frame
-    CGFloat width = [UIScreen mainScreen].bounds.size.width / _collectViewRowCount;
+    CGFloat width = [UIScreen mainScreen].bounds.size.width / self.colCount;
     CGFloat height = 50 + arc4random_uniform(20);
-    CGFloat x = width * (indexPath.row % _collectViewRowCount);
+    CGFloat x = width * (indexPath.row % self.colCount);
     
-    CGFloat y = [_originYAry[indexPath.row % _collectViewRowCount] floatValue];
-    _originYAry[indexPath.row % _collectViewRowCount] = @(height + y + 10);
-    layoutAttr.frame =CGRectMake(x, y, width, height);
+    CGFloat y = [self.originYArr[indexPath.row % self.colCount] floatValue];
+    self.originYArr[indexPath.row % self.colCount] = @(height + y + 10);
+    layoutAttr.frame = CGRectMake(x, y, width, height);
     return layoutAttr;
 }
- 
+
 // 3.返回collectionView的ContentSize
 // 由于瀑布流每一列的高度不一定，所以需要判读出最高的列作为height
 - (CGSize)collectionViewContentSize {
     // kvc获取最大值
-    CGFloat maxY = [[_originYAry valueForKeyPath:@"@max.floatValue"] floatValue];
+    CGFloat maxY = [[self.originYArr valueForKeyPath:@"@max.floatValue"] floatValue];
     return CGSizeMake([UIScreen mainScreen].bounds.size.width, maxY);
 }
- 
+
 // 4.Returns the layout attributes for all of the cells and views in the specified rectangle.
 -(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
-    return _attributeArray;
+    return self.attributeArr;
 }
 
 @end
