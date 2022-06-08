c3d3447180b4b7e82748d89ab41932f0303c0dd7
zhushanbo <zhushanbo@baidu.com>
2022-06-08 20:39:47 +0800

    no message

--- TestOC/Page/Mine/Common/PBCellHeight/Models/PBCellHeightFiveCellVM.h
+++ TestOC/Page/Mine/Common/PBCellHeight/Models/PBCellHeightFiveCellVM.h
@@ -10,15 +10,16 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// 字体
 #define labFont 15
 
 @class PBCellHeightZeroData;
 @interface PBCellHeightFiveCellVM : NSObject
 
-@property (nonatomic, assign) CGRect labRect;
-@property (nonatomic, assign) CGRect imageViewRect;
+@property (nonatomic, assign) CGRect labRect; // 标题的frame
+@property (nonatomic, assign) CGRect imageViewRect; // 图片的frame
 
-@property (nonatomic, assign) CGFloat cellHeight;
+@property (nonatomic, assign) CGFloat cellHeight; // cell的高度
 
 - (void)layoutInfoWithData:(PBCellHeightZeroData *)testListData;
 
