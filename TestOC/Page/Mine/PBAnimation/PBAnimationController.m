689230c819edd0416d9524546d5b5aa37efd5a5b
zhushanbo <zhushanbo@baidu.com>
2022-12-05 20:40:51 +0800

    no message

--- TestOC/Page/Mine/PBAnimation/PBAnimationController.m
+++ TestOC/Page/Mine/PBAnimation/PBAnimationController.m
@@ -206,7 +206,7 @@ NSArray *allSubviews(UIView *aView) {
     // https://www.yotrolz.com/posts/5fe4e0ec/
     // https://www.jianshu.com/p/c9cbbdaa9b02
     // https://blog.51cto.com/u_15127644/4057466
-    UIImage *image = [UIImage imageNamed:@"qipao_up_normal_blue"]; // qipao_up_normal_blue
+    UIImage *image = [UIImage imageNamed:@"qipao_up_normal_blue"]; // qipao_up_normal_blue、qipao_right_normal_blue
     CGFloat width = image.size.width;
     CGFloat height = image.size.height;
     NSLog(@"width = %lf, height = %lf", width, height); // 原始尺寸: width = 40.000000, height = 48.000000
