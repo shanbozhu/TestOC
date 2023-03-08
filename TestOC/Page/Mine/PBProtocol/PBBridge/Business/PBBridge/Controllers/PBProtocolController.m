51fbc8c5489a00a2818c0be394070a39a22e7196
zhushanbo <zhushanbo@baidu.com>
2023-03-08 12:52:20 +0800

    no message

--- TestOC/Page/Mine/PBProtocol/PBBridge/Business/PBBridge/Controllers/PBProtocolController.m
+++ TestOC/Page/Mine/PBProtocol/PBBridge/Business/PBBridge/Controllers/PBProtocolController.m
@@ -21,6 +21,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // 参考文档:https://blog.csdn.net/a18339063397/article/details/120891093
+    
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:btn];
     btn.frame = CGRectMake(100, 200, 50, 50);
