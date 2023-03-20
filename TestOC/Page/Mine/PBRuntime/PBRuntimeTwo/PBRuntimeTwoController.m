6d8cd4101c7adc0b94ed43e24ec8cc93d914aea9
zhushanbo <zhushanbo@baidu.com>
2023-03-20 17:29:28 +0800

    no message

--- TestOC/Page/Mine/PBRuntime/PBRuntimeTwo/PBRuntimeTwoController.m
+++ TestOC/Page/Mine/PBRuntime/PBRuntimeTwo/PBRuntimeTwoController.m
@@ -23,38 +23,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    unsigned int number;
-    objc_property_t *propertiList = class_copyPropertyList([self class], &number);
-    for (unsigned int i = 0; i < number; i++) {
-        const char *propertyName = property_getName(propertiList[i]);
-        NSLog(@"属性名称----%@", [NSString stringWithUTF8String:propertyName]);
+    {
+        unsigned int number;
+        objc_property_t *propertiList = class_copyPropertyList([self class], &number);
+        for (unsigned int i = 0; i < number; i++) {
+            const char *propertyName = property_getName(propertiList[i]);
+            NSLog(@"属性名称----%@", [NSString stringWithUTF8String:propertyName]);
+        }
+        free(propertiList);
     }
-    free(propertiList);
     
-    unsigned int methodCount;
-    Method *method = class_copyMethodList([self class], &methodCount);
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method me = method[i];
-        NSLog(@"方法名称----%@", NSStringFromSelector(method_getName(me)));
+    {
+        unsigned int methodCount;
+        Method *method = class_copyMethodList([self class], &methodCount);
+        for (unsigned int i = 0; i < methodCount; i++) {
+            Method me = method[i];
+            NSLog(@"方法名称----%@", NSStringFromSelector(method_getName(me)));
+        }
+        free(method);
     }
-    free(method);
     
-    unsigned int ivarCount;
-    Ivar *ivar = class_copyIvarList([self class], &ivarCount);
-    for (NSInteger index = 0; index < ivarCount; index++) {
-        const char *ivarName = ivar_getName(ivar[index]);
-        NSLog(@"成员变量名称----%@", [NSString stringWithUTF8String:ivarName]);
+    {
+        unsigned int ivarCount;
+        Ivar *ivar = class_copyIvarList([self class], &ivarCount);
+        for (NSInteger index = 0; index < ivarCount; index++) {
+            const char *ivarName = ivar_getName(ivar[index]);
+            NSLog(@"成员变量名称----%@", [NSString stringWithUTF8String:ivarName]);
+        }
+        free(ivar);
     }
-    free(ivar);
     
-    unsigned int protocalCount;
-    __unsafe_unretained Protocol **protocolList = class_copyProtocolList([self class], &protocalCount);
-    for (unsigned int i = 0; i< protocalCount; i++) {
-        Protocol *myProtocal = protocolList[i];
-        const char *protocolName = protocol_getName(myProtocal);
-        NSLog(@"协议名称----%@", [NSString stringWithUTF8String:protocolName]);
+    {
+        unsigned int protocalCount;
+        __unsafe_unretained Protocol **protocolList = class_copyProtocolList([self class], &protocalCount);
+        for (unsigned int i = 0; i< protocalCount; i++) {
+            Protocol *myProtocal = protocolList[i];
+            const char *protocolName = protocol_getName(myProtocal);
+            NSLog(@"协议名称----%@", [NSString stringWithUTF8String:protocolName]);
+        }
+        free(protocolList);
     }
-    free(protocolList);
 }
 
 
