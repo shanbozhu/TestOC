324e3556e02347717eda1e7fc766e4ea34f250fd
zhushanbo <zhushanbo@baidu.com>
2023-04-21 18:48:36 +0800

    no message

--- TestOC/Page/Mine/PBAFNetworking/Controllers/PBAFNetworkingZeroController.m
+++ TestOC/Page/Mine/PBAFNetworking/Controllers/PBAFNetworkingZeroController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     
     // 请求路径、请求参数
-    NSString *urlStr = @"https://m.baidu.com/suggest?empty_field=10000&ctl=his&action=list&cfrom=1099a&ds_lv=7&ds_stc=0.7740&from=1099a&matrixstyle=0&network=1_0&osbranch=i0&osname=baiduboxapp&puid=_u2Kt_OQv8SlmqqqB&service=bdbox&st=0&ua=828_1792_iphone_12.17.0.1_0&uid=45D34CA04432AE7FB8F806F7483DB2F06B58F8588FMMDBHJSRH&ut=iPhone11%2C8_12.1.2&zid=MowzQoEcY-nekbrCSe81hD2bEiGt5Q1itnKJo6S65D3FjREtnYW0pP9JS34FZAoGMXNM_cO7KynfHotkJEntNbw";
+    NSString *urlStr = @"https://mbd.baidu.com/icomment/v1/comment/rlist?appname=baiduboxlite&cfrom=1005640h&ds_lv=4&ds_stc=0.7740&from=1005640h&fv=13.30.0.10&matrixstyle=0&mps=154326807&mpv=1&network=1_0&sid=34836_3-8319_19556-56196_2-8313_19529-56785_2-56115_2-34064_2-35158_1-5760_9013-34999_8-35148_1-35262_2-107862_3-32205_2-56359_4-55371_1-35215_2-5280_7494-56512_4-9619_2-8321_19560-33923_6-9451_2-9618_2-8083_18570-5644_8666-56430_2-35223_1-5153_7043-34731_2-35072_2-56076_3&st=0&ua=828_1792_iphone_6.1.0.3_0&uid=45D34CA04432AE7FB8F806F7483DB2F06B58F8588FMMDBHJSRH&zid=Nz1vfc_o7oN3ci-TIwM1lwW9-GqQg2jHJyLNp9nVbRIFAsQJxD06HMqQTcbu6Y9x0StTFnWsNpHkiJhkxPtHb6Q&sdkversion=1.1.2";
     
     {
         NSURL *url = [NSURL URLWithString:[urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
@@ -90,7 +90,19 @@
     
     // 请求体
     NSMutableDictionary *paras = [NSMutableDictionary dictionary];
-    [paras setValue:@"0" forKey:@"sortTime"];
+    [paras setValue:@"1000000056224844" forKey:@"topic_id"];
+    [paras setValue:@"channel_video_landing" forKey:@"source"];
+    [paras setValue:@"0" forKey:@"start"];
+    [paras setValue:@"baidumedia" forKey:@"source_type"];
+    [paras setValue:@"1122518916369402407" forKey:@"reply_id"];
+    [paras setValue:@"38383331323433303437373531393733353736" forKey:@"request_id"];
+    [paras setValue:@"1762683039161549402" forKey:@"key"];
+    [paras setValue:@"20" forKey:@"num"];
+    NSMutableDictionary *extdata = [NSMutableDictionary dictionary];
+    [extdata setValue:@"feed" forKey:@"origin"];
+    [extdata setValue:@"E18940390F8EC74785570C5BE86236F8" forKey:@"client_logid"];
+    [extdata setValue:@"" forKey:@"s_session"];
+    [paras setValue:extdata forKey:@"extdata"];
     
     return paras;
 }
