1af74b0f2db6a6710b5edfcfeab4d2ff87412d3f
zhushanbo <zhushanbo@baidu.com>
2024-04-17 21:20:35 +0800

    no message

---
+++ TestOC/Page/Mine/PBRuntime/PBRuntimeZero/PBRuntimeZeroDebugController.h
@@ -0,0 +1,17 @@
+//
+//  PBRuntimeZeroDebugController.h
+//  TestOC
+//
+//  Created by shanbo on 2024/4/15.
+//  Copyright © 2024 DaMaiIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PBRuntimeZeroDebugController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestOC/Page/Mine/PBRuntime/PBRuntimeZero/PBRuntimeZeroDebugController.m
@@ -0,0 +1,32 @@
+//
+//  PBRuntimeZeroDebugController.m
+//  TestOC
+//
+//  Created by shanbo on 2024/4/15.
+//  Copyright © 2024 DaMaiIOS. All rights reserved.
+//
+
+#import "PBRuntimeZeroDebugController.h"
+
+@interface PBRuntimeZeroDebugController ()
+
+@end
+
+@implementation PBRuntimeZeroDebugController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
