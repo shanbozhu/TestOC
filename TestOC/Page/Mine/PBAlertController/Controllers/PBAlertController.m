abc4887c836a143b28b3b85c834e8fb2def96517
zhushanbo <zhushanbo@baidu.com>
2024-04-18 19:31:59 +0800

    no message

--- TestOC/Page/Mine/PBAlertController/Controllers/PBAlertController.m
+++ TestOC/Page/Mine/PBAlertController/Controllers/PBAlertController.m
@@ -7,6 +7,7 @@
 //
 
 #import "PBAlertController.h"
+#include <execinfo.h> /* for backtrace() */
 
 @interface PBAlertController ()
 
@@ -147,6 +148,32 @@
     
     // 打印所有子视图
     [self getSub:alert.view andLevel:1];
+    
+    
+    NSLog(@"<<<<%@>>>>", [self.class getSymbolsWithDepth:1000]);
+}
+
++ (NSString *)getSymbolsWithDepth:(int)size {
+    vm_address_t *stacks[size];
+    size_t depth = backtrace((void**)stacks, size);
+    NSMutableString *stackInfo = [[NSMutableString alloc] init];
+    NSDateFormatter* df1 = [NSDateFormatter new];
+    df1.dateFormat = @"yyyy-MM-dd HH:mm:ss.SSS";
+    NSString *dateStr1 = [df1 stringFromDate:[NSDate date]];
+    [stackInfo appendFormat:@"%@\n", dateStr1];
+    
+    char **strings;
+    strings = backtrace_symbols((void**)stacks, (int)depth);
+    if (strings == NULL) {
+        return NULL;
+    }
+    for (int j = 0; j < depth; j++) {
+        NSString *str=  [[NSString alloc] initWithUTF8String:strings[j]];
+        [stackInfo appendFormat:@"%@\n", str];
+    }
+    free(strings);
+    
+    return stackInfo;
 }
 
 - (void)logViewHierarchy:(UIView *)view {
