c7c61e49ec496d6ecc2b7fa75ecf39e159b1ad7e
zhushanbo <zhushanbo@baidu.com>
2024-04-18 12:12:30 +0800

    no message

--- TestOC/Page/Mine/PBRuntime/PBRuntimeOne/PBRuntimeOneController.m
+++ TestOC/Page/Mine/PBRuntime/PBRuntimeOne/PBRuntimeOneController.m
@@ -26,13 +26,11 @@
 #pragma mark -
 
 + (NSArray <Class> *)pb_bundleOwnClassesInfo {
-    
     NSMutableArray *resultArray = [NSMutableArray array];
     
     unsigned int classCount;
     const char **classes;
     Dl_info info;
-    
     dladdr(&_mh_execute_header, &info);
     classes = objc_copyClassNamesForImage(info.dli_fname, &classCount);
     
@@ -44,20 +42,17 @@
         [resultArray addObject:class];
         dispatch_semaphore_signal(semaphore);
     });
-    
     return resultArray.mutableCopy;
 }
 
 + (NSArray <NSString *> *)pb_bundleAllClassesInfo {
-    
     NSMutableArray *resultArray = [NSMutableArray new];
-
+    
     int classCount = objc_getClassList(NULL, 0);
-
     Class *classes = NULL;
     classes = (__unsafe_unretained Class *)malloc(sizeof(Class) *classCount);
     classCount = objc_getClassList(classes, classCount);
-
+    
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(1);
     dispatch_apply(classCount, dispatch_get_global_queue(0, 0), ^(size_t index) {
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
@@ -66,9 +61,7 @@
         [resultArray addObject:className];
         dispatch_semaphore_signal(semaphore);
     });
-
     free(classes);
-    
     return resultArray.mutableCopy;
 }
 
