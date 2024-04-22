9e048c3eae7896c82d4d27c398f9546634217145
zhushanbo <zhushanbo@baidu.com>
2024-04-22 17:38:44 +0800

    no message

--- TestOC/Page/Mine/PBRuntime/PBRuntimeSix/PBRuntimeSixController.m
+++ TestOC/Page/Mine/PBRuntime/PBRuntimeSix/PBRuntimeSixController.m
@@ -24,9 +24,9 @@
          3.1进行方法签名；NSMethodSignature的两个参数：numberOfArguments方法参数的个数；methodReturnLength方法返回值类型
          的长度，大于0表示有返回值
          3.2初始化NSInvocation并设置参数
-         第一个参数下标为0是target，及响应者；
-         第二个参数是selector，及需要调用的方法；
-         第三个起是自定义参数，必须传递参数的地址，不能直接传值，例如：str1，str2，str3
+            第一个参数下标为0是target，及响应者；
+            第二个参数是selector，及需要调用的方法；
+            第三个起是自定义参数，必须传递参数的地址，不能直接传值，例如：str1，str2，str3
          3.3获取返回值；可以在调用invoke前，也可以在invoke之后
          */
         SEL selector = @selector(runA:b:c:);
@@ -42,10 +42,11 @@
         [invocation setArgument:&arg2 atIndex:3];
         [invocation setArgument:&arg3 atIndex:4];
         [invocation invoke];
+        NSLog(@"执行1. sign.numberOfArguments = %ld, sign.methodReturnLength = %ld", sign.numberOfArguments, sign.methodReturnLength);
         if (sign.methodReturnLength > 0) {
             NSString *result = nil;
             [invocation getReturnValue:&result];
-            NSLog(@"result = %@", result);
+            NSLog(@"执行3. result = %@", result);
         } else {
             NSLog(@"没有返回值");
         }
@@ -53,7 +54,7 @@
 }
 
 - (NSString *)runA:(NSString *)a b:(NSString *)b c:(NSString *)c {
-    NSLog(@"----run-----, a: %@, b: %@, c: %@", a, b, c);
+    NSLog(@"执行2. ----run-----, a: %@, b: %@, c: %@", a, b, c);
     return @"NSInvocation Succeed!";
 }
 
