7dad3fa7c5c3e3bb48f6e0f883ce6c044ef4e79d
zhushanbo <zhushanbo@baidu.com>
2024-05-09 21:24:44 +0800

    no message

--- TestOC/Page/Mine/PBNSLog/PBNSLogController.m
+++ TestOC/Page/Mine/PBNSLog/PBNSLogController.m
@@ -39,10 +39,37 @@
     NSLog(@"height = %.3lf", height); // 18.012
     
     //
-    NSInteger aa = 500;
+    NSUInteger aa = 500;
     NSLog(@"aa = %lx", aa); // 1f4 = 4 * 16^0 + f * 16^1 + 1 * 16^2 = 500
     NSLog(@"aa = %lX", aa); // 1F4
     NSLog(@"aa = %lo", aa); // 764 = 4 * 8^0 + 6 * 8^1 + 7 * 8^2 = 500
+    
+    //
+    printBinary(aa);
+    for (int i = 0; i < 16; i++) {
+        printBinary(i);
+        printf("\n");
+    }
+}
+
+/**
+void printBinary(unsigned int num) {
+    NSMutableString *binaryString = [NSMutableString string];
+    for (int i = 31; i >= 0; i--) {
+        [binaryString appendString:((num & (1 << i)) ? @"1" : @"0")];
+        if (i % 4 == 0) {
+            [binaryString appendString:@" "];
+        }
+    }
+    NSLog(@"Binary representation of %d is: %@", num, binaryString);
+}
+ */
+
+void printBinary(unsigned int num) {
+    if (num > 1) {
+        printBinary(num >> 1);
+    }
+    printf("%d", num & 1);
 }
 
 @end
