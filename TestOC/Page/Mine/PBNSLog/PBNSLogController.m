bb2b9afd9426fd44b9a43be6bf35312c5edd6ac2
zhushanbo <zhushanbo@baidu.com>
2024-05-11 19:19:30 +0800

    no message

--- TestOC/Page/Mine/PBNSLog/PBNSLogController.m
+++ TestOC/Page/Mine/PBNSLog/PBNSLogController.m
@@ -85,17 +85,16 @@
     NSLog(@"[str bdp_md5Hash] = %@", [str bdp_md5Hash]); // 420e57b017066b44e05ea1577f6e2e12
     NSLog(@"[str bdp_sha1Hash] = %@", [str bdp_sha1Hash]); // 3c608e47152c7b175e9d3c171002dc234bb00953
     
+    NSString *filePath = [PBSandBox absolutePathWithRelativePath:@"/Documents/PBStorage/PBStorageStr"];
+    NSLog(@"[NSString bdp_fileMD5HashCreateWithPath:filePath] = %@", [NSString bdp_fileMD5HashCreateWithPath:filePath]);
+    NSLog(@"[FileHash md5HashOfFileAtPath:filePath] = %@", [FileHash md5HashOfFileAtPath:filePath]);
+    
     // base64
     NSString *base64Str = [[str dataUsingEncoding:NSUTF8StringEncoding] bdp_base64Encoding];
     NSLog(@"base64Str = %@", base64Str); // aGVsbG93b3JsZCE=
     NSData *data = [NSData bdp_dataWithBase64EncodedString:base64Str];
     NSString *originStr = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"originStr = %@", originStr); // HELLOWORLD
-    
-    //
-    NSString *filePath = [PBSandBox absolutePathWithRelativePath:@"/Documents/PBStorage/PBStorageStr"];
-    NSLog(@"[NSString bdp_fileMD5HashCreateWithPath:filePath] = %@", [NSString bdp_fileMD5HashCreateWithPath:filePath]);
-    NSLog(@"[FileHash md5HashOfFileAtPath:filePath] = %@", [FileHash md5HashOfFileAtPath:filePath]);
 }
 
 /**
