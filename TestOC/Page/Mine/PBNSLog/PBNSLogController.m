47823584412824995f822e0c2118109f10622cd3
zhushanbo <zhushanbo@baidu.com>
2024-06-14 10:10:58 +0800

    no message

--- TestOC/Page/Mine/PBNSLog/PBNSLogController.m
+++ TestOC/Page/Mine/PBNSLog/PBNSLogController.m
@@ -63,43 +63,45 @@
         }
     }
     
-    // 十进制转k进制:除k取余法
-    // k进制转十进制:乘以k的次方
-    
-    // 有效数字:从第一个不是0的数开始,以下都是
-    // 00000018.012的有效数字是5
-    // 至多保留10位有效数字:%.10g
-    
-    // 二进制:0b1111
-    // 十六进制:0x1f4
-    // 八进制:0o764
-    
-    // 十六进制小写:%x
-    // 十六进制大写:%X
-    // 八进制:%o
-    
-    // 总共至少显示12位,前面不足补0,大于12位显示实际位数,小数点后显示3位小数:%012.3lf
-    
-    //
-    CGFloat height = 18.0123456789;
-    NSLog(@"height = %lf", height); // 18.012346
-    NSLog(@"height = %012.3lf", height); // 00000018.012
-    NSLog(@"height = %0.3lf", height); // 18.012
-    NSLog(@"height = %.3lf", height); // 18.012
-    NSLog(@"height = %e", height); // 1.801235e+01
-    NSLog(@"height = %.10g", height); // 18.01234568
-    
-    //
-    NSInteger aa = 500;
-    NSLog(@"aa = %lx", aa); // 1f4 = 4 * 16^0 + f * 16^1 + 1 * 16^2 = 500
-    NSLog(@"aa = %lX", aa); // 1F4
-    NSLog(@"aa = %lo", aa); // 764 = 4 * 8^0 + 6 * 8^1 + 7 * 8^2 = 500
-    
-    //
-    printBinary(aa);
-    for (int i = 0; i < 16; i++) {
-        printBinary(i);
-        printf("\n");
+    {
+        // 十进制转k进制:除k取余法
+        // k进制转十进制:乘以k的次方
+        
+        // 有效数字:从第一个不是0的数开始,以下都是
+        // 00000018.012的有效数字是5
+        // 至多保留10位有效数字:%.10g
+        
+        // 二进制:0b1111
+        // 十六进制:0x1f4
+        // 八进制:0o764
+        
+        // 十六进制小写:%x
+        // 十六进制大写:%X
+        // 八进制:%o
+        
+        // 总共至少显示12位,前面不足补0,大于12位显示实际位数,小数点后显示3位小数:%012.3lf
+        
+        //
+        CGFloat height = 18.0123456789;
+        NSLog(@"height = %lf", height); // 18.012346
+        NSLog(@"height = %012.3lf", height); // 00000018.012
+        NSLog(@"height = %0.3lf", height); // 18.012
+        NSLog(@"height = %.3lf", height); // 18.012
+        NSLog(@"height = %e", height); // 1.801235e+01
+        NSLog(@"height = %.10g", height); // 18.01234568
+        
+        //
+        NSInteger aa = 500;
+        NSLog(@"aa = %lx", aa); // 1f4 = 4 * 16^0 + f * 16^1 + 1 * 16^2 = 500
+        NSLog(@"aa = %lX", aa); // 1F4
+        NSLog(@"aa = %lo", aa); // 764 = 4 * 8^0 + 6 * 8^1 + 7 * 8^2 = 500
+        
+        //
+        printBinary(aa);
+        for (int i = 0; i < 16; i++) {
+            printBinary(i);
+            printf("\n");
+        }
     }
     
     /**
