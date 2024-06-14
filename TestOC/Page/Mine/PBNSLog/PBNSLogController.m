811012a89c6694bbe4b98b6a0e1f1322a35f9557
zhushanbo <zhushanbo@baidu.com>
2024-06-14 10:14:31 +0800

    no message

--- TestOC/Page/Mine/PBNSLog/PBNSLogController.m
+++ TestOC/Page/Mine/PBNSLog/PBNSLogController.m
@@ -104,76 +104,78 @@
         }
     }
     
-    /**
-     -> PBStorage 10:52:02
-      $ pwd
-     /Users/wsc/Library/Developer/CoreSimulator/Devices/6AED87EE-9937-4DF2-936B-A6E41A4F751E/data/Containers/Data/Application/DF8EFF49-AC1C-4588-9EF1-DA06D4BF0A80/Documents/PBStorage
-     -> PBStorage 10:52:16
-      $ ls
-     PBStorageArr    PBStorageData    PBStorageDict    PBStorageStr
-     -> PBStorage 10:52:23
-      $ cat PBStorageStr
-     helloworld!-> PBStorage 10:52:31
-      $ md5 PBStorageStr
-     MD5 (PBStorageStr) = 420e57b017066b44e05ea1577f6e2e12
-     -> PBStorage 10:52:40
-      $ shasum PBStorageStr
-     3c608e47152c7b175e9d3c171002dc234bb00953  PBStorageStr
-     -> PBStorage 10:52:48
-      $ shasum -a 1 PBStorageStr
-     3c608e47152c7b175e9d3c171002dc234bb00953  PBStorageStr
-     -> PBStorage 10:52:57
-      $ shasum -a 256 PBStorageStr
-     98d234db7e91f5ba026a25d0d6f17bc5ee0a347ea2216b0c9de06d43536d49f4  PBStorageStr
-     -> PBStorage 10:53:07
-      $ shasum -a 512 PBStorageStr
-     4e6be41aade78bebbe95662312b581088bd860320ed99cfbe5ae8ab8cf355e95f6bac60220bb0dee2d66613111c18f8ce08319d014fbc07e74001693172551c1  PBStorageStr
-     */
-    // md5  32个字符/2=16byte 16*8=128bit
-    // sha1 40个字符/2=20byte 20*8=160bit
-    NSString *str = HELLOWORLD;
-    NSLog(@"[str bdp_md5Hash] = %@", [str bdp_md5Hash]); // 420e57b017066b44e05ea1577f6e2e12
-    NSLog(@"[str bdp_sha1Hash] = %@", [str bdp_sha1Hash]); // 3c608e47152c7b175e9d3c171002dc234bb00953
-    
-    NSString *filePath = [PBSandBox absolutePathWithRelativePath:kPBSTORAGESTR];
-    NSLog(@"[NSString bdp_fileMD5HashCreateWithPath:filePath] = %@", [NSString bdp_fileMD5HashCreateWithPath:filePath]); // 420e57b017066b44e05ea1577f6e2e12
-    NSLog(@"[FileHash md5HashOfFileAtPath:filePath] = %@", [FileHash md5HashOfFileAtPath:filePath]); // 420e57b017066b44e05ea1577f6e2e12
-    NSString *fileContentStr = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
-    NSLog(@"[fileContentStr bdp_md5Hash] = %@", [fileContentStr bdp_md5Hash]); // 420e57b017066b44e05ea1577f6e2e12
-    
-    // base64
-    NSString *base64Str = [[str dataUsingEncoding:NSUTF8StringEncoding] bdp_base64Encoding];
-    NSLog(@"base64Str = %@", base64Str); // aGVsbG93b3JsZCE=
-    NSData *data = [NSData bdp_dataWithBase64EncodedString:base64Str];
-    NSString *originStr = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"originStr = %@", originStr); // HELLOWORLD
-    
-    // urlencode
-    NSString *urlStr = kPBBaiduMap;
-    NSLog(@"[urlStr bdp_percentEncoding] = %@", [urlStr bdp_percentEncoding]);
-    NSLog(@"[urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]] = %@", [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]);
-    NSLog(@"[urlStr bdp_encodeURIComponent] = %@", [urlStr bdp_encodeURIComponent]);
-    NSLog(@"[urlStr bdp_decodeURIComponent] = %@", [urlStr bdp_decodeURIComponent]);
-    NSLog(@"[[urlStr bdp_encodeURIComponent] bdp_decodeURIComponent] = %@", [[urlStr bdp_encodeURIComponent] bdp_decodeURIComponent]);
-    
-    // Unicode
-    NSString *contentString = @"你好世界ABC";
-    NSMutableString *unicodeStr = [NSMutableString string];
-    for (int i = 0; i < contentString.length; i++) {
-        unichar character = [contentString characterAtIndex:i];
-        [unicodeStr appendFormat:@"\\u%04x", character];
+    {
+        /**
+         -> PBStorage 10:52:02
+          $ pwd
+         /Users/wsc/Library/Developer/CoreSimulator/Devices/6AED87EE-9937-4DF2-936B-A6E41A4F751E/data/Containers/Data/Application/DF8EFF49-AC1C-4588-9EF1-DA06D4BF0A80/Documents/PBStorage
+         -> PBStorage 10:52:16
+          $ ls
+         PBStorageArr    PBStorageData    PBStorageDict    PBStorageStr
+         -> PBStorage 10:52:23
+          $ cat PBStorageStr
+         helloworld!-> PBStorage 10:52:31
+          $ md5 PBStorageStr
+         MD5 (PBStorageStr) = 420e57b017066b44e05ea1577f6e2e12
+         -> PBStorage 10:52:40
+          $ shasum PBStorageStr
+         3c608e47152c7b175e9d3c171002dc234bb00953  PBStorageStr
+         -> PBStorage 10:52:48
+          $ shasum -a 1 PBStorageStr
+         3c608e47152c7b175e9d3c171002dc234bb00953  PBStorageStr
+         -> PBStorage 10:52:57
+          $ shasum -a 256 PBStorageStr
+         98d234db7e91f5ba026a25d0d6f17bc5ee0a347ea2216b0c9de06d43536d49f4  PBStorageStr
+         -> PBStorage 10:53:07
+          $ shasum -a 512 PBStorageStr
+         4e6be41aade78bebbe95662312b581088bd860320ed99cfbe5ae8ab8cf355e95f6bac60220bb0dee2d66613111c18f8ce08319d014fbc07e74001693172551c1  PBStorageStr
+         */
+        // md5  32个字符/2=16byte 16*8=128bit
+        // sha1 40个字符/2=20byte 20*8=160bit
+        NSString *str = HELLOWORLD;
+        NSLog(@"[str bdp_md5Hash] = %@", [str bdp_md5Hash]); // 420e57b017066b44e05ea1577f6e2e12
+        NSLog(@"[str bdp_sha1Hash] = %@", [str bdp_sha1Hash]); // 3c608e47152c7b175e9d3c171002dc234bb00953
+        
+        NSString *filePath = [PBSandBox absolutePathWithRelativePath:kPBSTORAGESTR];
+        NSLog(@"[NSString bdp_fileMD5HashCreateWithPath:filePath] = %@", [NSString bdp_fileMD5HashCreateWithPath:filePath]); // 420e57b017066b44e05ea1577f6e2e12
+        NSLog(@"[FileHash md5HashOfFileAtPath:filePath] = %@", [FileHash md5HashOfFileAtPath:filePath]); // 420e57b017066b44e05ea1577f6e2e12
+        NSString *fileContentStr = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+        NSLog(@"[fileContentStr bdp_md5Hash] = %@", [fileContentStr bdp_md5Hash]); // 420e57b017066b44e05ea1577f6e2e12
+        
+        // base64
+        NSString *base64Str = [[str dataUsingEncoding:NSUTF8StringEncoding] bdp_base64Encoding];
+        NSLog(@"base64Str = %@", base64Str); // aGVsbG93b3JsZCE=
+        NSData *data = [NSData bdp_dataWithBase64EncodedString:base64Str];
+        NSString *originStr = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"originStr = %@", originStr); // HELLOWORLD
+        
+        // urlencode
+        NSString *urlStr = kPBBaiduMap;
+        NSLog(@"[urlStr bdp_percentEncoding] = %@", [urlStr bdp_percentEncoding]);
+        NSLog(@"[urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]] = %@", [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]);
+        NSLog(@"[urlStr bdp_encodeURIComponent] = %@", [urlStr bdp_encodeURIComponent]);
+        NSLog(@"[urlStr bdp_decodeURIComponent] = %@", [urlStr bdp_decodeURIComponent]);
+        NSLog(@"[[urlStr bdp_encodeURIComponent] bdp_decodeURIComponent] = %@", [[urlStr bdp_encodeURIComponent] bdp_decodeURIComponent]);
+        
+        // Unicode
+        NSString *contentString = @"你好世界ABC";
+        NSMutableString *unicodeStr = [NSMutableString string];
+        for (int i = 0; i < contentString.length; i++) {
+            unichar character = [contentString characterAtIndex:i];
+            [unicodeStr appendFormat:@"\\u%04x", character];
+        }
+        NSLog(@"unicodeStr = %@", unicodeStr);
+        
+        NSString *aaaa = @"\u4f60\u597d\u4e16\u754c";
+        NSLog(@"aaaa = %@", aaaa);
+        UILabel *aaaaLab = [[UILabel alloc] init];
+        [self.view addSubview:aaaaLab];
+        aaaaLab.frame = CGRectMake(100, 150, 200, 100);
+        aaaaLab.numberOfLines = 0;
+        aaaaLab.text = aaaa;
+        aaaaLab.layer.borderWidth = 1.1;
+        aaaaLab.layer.borderColor = [UIColor redColor].CGColor;
     }
-    NSLog(@"unicodeStr = %@", unicodeStr);
-    
-    NSString *aaaa = @"\u4f60\u597d\u4e16\u754c";
-    NSLog(@"aaaa = %@", aaaa);
-    UILabel *aaaaLab = [[UILabel alloc] init];
-    [self.view addSubview:aaaaLab];
-    aaaaLab.frame = CGRectMake(100, 150, 200, 100);
-    aaaaLab.numberOfLines = 0;
-    aaaaLab.text = aaaa;
-    aaaaLab.layer.borderWidth = 1.1;
-    aaaaLab.layer.borderColor = [UIColor redColor].CGColor;
 }
 
 void printBinary(long num) {
