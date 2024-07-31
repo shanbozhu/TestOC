20cfe8d8ad754a1e8829d8633af788252d1c30f6
zhushanbo <zhushanbo@baidu.com>
2024-07-31 22:00:53 +0800

    no message

--- TestOC/Page/Mine/PBSwift/PBSwiftController.swift
+++ TestOC/Page/Mine/PBSwift/PBSwiftController.swift
@@ -24,6 +24,16 @@ class PBSwiftController: PBBaseController {
         myString = nil
         print(myString) // nil
         //print(myString!) // 对nil解包，会崩溃。
+        if myString != nil {
+            let tmp = myString!
+            print(tmp)
+        }
+        if let tmp = myString {
+            print(tmp)
+        }
+        myString = "hello"
+        let tmp: String = myString!
+        print(tmp)
         
         // 可选隐式解包
         myString1 = "world"
@@ -32,10 +42,22 @@ class PBSwiftController: PBBaseController {
         myString1 = nil
         print(myString1) // nil
         //print(myString1!) // 对nil解包，会崩溃。Fatal error: Unexpectedly found nil while unwrapping an Optional value: file TestOC/PBSwiftController.swift, line 36
+        if myString1 != nil {
+            let tmp = myString1!
+            print(tmp)
+        }
+        if let tmp = myString1 {
+            print(tmp)
+        }
+        myString1 = "world"
+        let tmp1: String = myString1
+        print(tmp1)
+        let tmp11: String = myString1! // !叹号可加可不加
+        print(tmp11)
         
         // 非nil字符串
         myString2 = "world"
-        print(myString2) // Optional("world")
+        print(myString2) // world
         //myString2 = nil // 'nil' cannot be assigned to type 'String'
     }
     
