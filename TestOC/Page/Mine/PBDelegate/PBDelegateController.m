1c8eb22d00c14b3d88e7615558a752924ecbea67
zhushanbo <zhushanbo@baidu.com>
2024-11-25 10:56:27 +0800

    no message

--- TestOC/Page/Mine/PBDelegate/PBDelegateController.m
+++ TestOC/Page/Mine/PBDelegate/PBDelegateController.m
@@ -15,20 +15,20 @@
 /**
  novel依赖novelcore
  novel依赖growth
- novelcore比growth还下层
+ novelcore比growth还底层
 
  问：novelcore需要调用growth，如何处理？
  答：novelcore利用delegate反向调用novel，novel调用growth。
 
  PS：
- 利用delegate或block反向调用，只能调用行为，行为传系统类型或下层类型，无法传上层类型。因为下层不依赖上层，所以无法使用上层类型。
- 调用在下，定义在上。
+ 利用delegate或block反向调用，只能调用行为，行为传系统类型或底层类型，无法传顶层类型。因为底层不依赖顶层，所以无法使用顶层类型。
+ 底层调用，顶层实现(定义)。
 
  问：novelcore需要使用growth中的类型，如何处理？
  答：novelcore利用delegate反向调用novel，novel调用growth。在novel中将growth的类型转换为novelcore的类型后返回。相当于novelcore使用growth中的类型。
 
  PS：
- 若下层非要使用上层类型且不好将上层类型下沉，可以调用行为传下层类型后，在上层将上层类型类型转换为下层类型后返回。
+ 若底层非要使用顶层类型且不好将顶层类型下沉，可以调用行为传底层类型后，在顶层将顶层类型类型转换为底层类型后返回。
  */
 
 @implementation PBDelegateController
